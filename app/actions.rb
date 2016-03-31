07daa763c35060d0181dd2a6fd9cb8c6d93ff076
Terit <artheriault@gmail.com>
2016-03-30 21:17:27 -0700

    log tag

--- app/actions.rb
+++ app/actions.rb
@@ -8,7 +8,8 @@ options "*" do
 end
 
 post '/api/leaderboard/new' do
-  tag = params[:tag] || 'popular'
+  tag = params[:tag]
+  puts params[:tag]
   leader = Leader.create(name: params[:name], hashtag: tag, score: params[:score].to_i)
   leaders = Leader.where(hashtag: tag).order(score: :desc).pluck(:id)
   { rank: (leaders.index(leader.id) + 1) }.to_json
